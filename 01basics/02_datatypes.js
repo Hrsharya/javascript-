29d49c6a61e5c9c58e8007891ecc8dfc28b35568
Hrsharya <harsharya9301@gmail.com>
2026-06-20 20:32:04 +0000

    datatypes

---
+++ 01basics/02_datatypes.js
@@ -0,0 +1,6 @@
+"use strict";//treat as new standard
+// alert(3+1) it can run in browser as browser has js script engine but we are using js
+//null==> standalone value
+//bigint==> for higher values
+//undefined==>value id=s not defined
+//symbol==> unique
